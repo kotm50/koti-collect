1fccd94cb1ecbe6c5b71e38fafc1806ae79284e4
kotm50 <koreatm50@gmail.com>
2023-11-09 16:28:19 +0900

    action test

--- src/Components/CollectIndex.js
+++ src/Components/CollectIndex.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import axios from "axios";
+
 function CollectIndex() {
+  useEffect(() => {
+    getGoods();
+    //eslint-next-line
+  }, []);
+  const getGoods = async (c, b, p) => {
+    let listUrl = "/api/v1/shop/goods/list";
+    const data = {
+      page: p,
+      size: 1,
+    };
+    await axios
+      .get(listUrl, {
+        params: data,
+      })
+      .then(async res => {
+        console.log(res.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
   return (
     <>
       <div className="fixed left-1/2 top-1/2 w-fit h-fit -translate-x-1/2 -translate-y-1/2">
