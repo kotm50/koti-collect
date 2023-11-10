a82e049e4595016d4d65a26d3c4461bb8a211954
kotm50 <koreatm50@gmail.com>
2023-11-10 11:31:20 +0900

    로그아웃 버튼 아래로

--- src/Components/Collect/Collect.js
+++ src/Components/Collect/Collect.js
@@ -24,27 +24,26 @@ function Collect() {
   };
   return (
     <>
-      <div className="fixed top-0 left-0 w-[240px] h-screen py-4 border-r bg-white shadow-lg">
-        <h2 className="font-bold p-4 text-xl text-center">
-          수금전산 테스트페이지
-        </h2>
-        <div className="flex flex-col justify-start divide-y border-y">
-          <div className="p-4">메인</div>
-          <div className="p-4">미수금 입력</div>
-          <div className="p-4">수금 입력</div>
-          <div className="p-4">월별 내역보기</div>
-          <div className="p-4">기프티콘 충전현황</div>
+      <div className="fixed top-0 left-0 w-[240px] h-screen py-4 border-r bg-white shadow-lg flex flex-col justify-between">
+        <div>
+          <h2 className="font-bold p-4 text-xl text-center">
+            수금전산 테스트페이지
+          </h2>
+          <div className="flex flex-col justify-start divide-y border-y">
+            <div className="p-4">메인</div>
+            <div className="p-4">미수금 입력</div>
+            <div className="p-4">수금 입력</div>
+            <div className="p-4">월별 내역보기</div>
+            <div className="p-4">기프티콘 충전현황</div>
+          </div>
         </div>
-        <div className="flex flex-col justify-end">
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white p-2 w-full"
-            onClick={logout}
-          >
-            로그아웃
-          </button>
-        </div>
+        <button
+          className="bg-gray-500 hover:bg-gray-700 text-white p-2 w-full"
+          onClick={logout}
+        >
+          로그아웃
+        </button>
       </div>
-
       <div className="pl-[250px]">
         <Outlet />
       </div>
