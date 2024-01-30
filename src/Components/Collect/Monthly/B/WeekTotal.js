81b92cf4834d158bee0da1c9809e1a8a2b11dfb5
kotm50 <koreatm50@gmail.com>
2024-01-30 17:40:03 +0900

    되는데

--- src/Components/Collect/Monthly/B/WeekTotal.js
+++ src/Components/Collect/Monthly/B/WeekTotal.js
@@ -1,9 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function WeekTotal(props) {
-  useEffect(() => {
-    console.log(props.weekList);
-  }, [props.weekList]);
   return (
     <div className="bg-white p-4 rounded-lg drop-shadow">
       <h3 className="text-lg mb-2 font-bold">주차별 수수료 결제</h3>
