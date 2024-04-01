76d5719e532a98476d44bfa28cee7f5f4e74f41d
kotm50 <koreatm50@gmail.com>
2024-04-01 17:30:36 +0900

    광고제거

--- src/Components/Layout/Menu.js
+++ src/Components/Layout/Menu.js
@@ -105,12 +105,6 @@ function Menu(props) {
         className={`bg-gray-100 flex flex-col justify-start divide-y border-y`}
       >
         <Link
-          to="/board/list/B05"
-          className={`px-3 py-2 text-sm hover:bg-gray-200 hover:text-rose-500 transition-all duration-300`}
-        >
-          광고 게시판
-        </Link>
-        <Link
           to="/board/list/B04"
           className={`px-3 py-2 text-sm hover:bg-gray-200 hover:text-rose-500 transition-all duration-300`}
         >
